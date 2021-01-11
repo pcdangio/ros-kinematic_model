a780671fd1591287129056f6004826c77d1656a2
Paul D'Angio <pcdangio@gmail.com>
2021-01-11 00:10:23 -0500

    transform: added identity constructor and inverse method

--- include/kinematic_model/geometry/transform.h
+++ include/kinematic_model/geometry/transform.h
@@ -11,13 +11,16 @@ namespace geometry {
 class transform_t
 {
 public:
+    transform_t();
     transform_t(const Eigen::Vector3d& translation, const Eigen::Quaterniond& rotation);
     transform_t(const Eigen::Vector3d& translation);
     transform_t(const Eigen::Quaterniond& rotation);
 
-    void transform(transform_t& transform);
-    void transform(pose_t& pose);
+    void transform(transform_t& transform) const;
+    void transform(pose_t& pose) const;
 
+    transform_t inverse() const;
+    
 private:
     Eigen::Vector3d m_translation;
     Eigen::Quaterniond m_rotation;
--- src/kinematic_model/geometry/transform.cpp
+++ src/kinematic_model/geometry/transform.cpp
@@ -3,6 +3,11 @@
 using namespace kinematic_model::geometry;
 
 // CONSTRUCTORS
+transform_t::transform_t()
+{
+    transform_t::m_translation.setZero();
+    transform_t::m_rotation.setIdentity();
+}
 transform_t::transform_t(const Eigen::Vector3d& translation, const Eigen::Quaterniond& rotation)
 {
     transform_t::m_translation = translation;
@@ -20,7 +25,7 @@ transform_t::transform_t(const Eigen::Quaterniond& rotation)
 }
 
 // METHODS
-void transform_t::transform(transform_t& transform)
+void transform_t::transform(transform_t& transform) const
 {
     // Apply this transform's rotation to the original transform.
     transform.m_rotation = transform_t::m_rotation * transform.m_rotation;
@@ -31,7 +36,7 @@ void transform_t::transform(transform_t& transform)
     // Add this transform's translation to the now rotated original translation.
     transform.m_translation += transform_t::m_translation;
 }
-void transform_t::transform(pose_t& pose)
+void transform_t::transform(pose_t& pose) const
 {
     // Rotate the original pose.
     pose.m_orientation = transform_t::m_rotation * pose.m_orientation;
@@ -41,4 +46,18 @@ void transform_t::transform(pose_t& pose)
 
     // Translate the rotated pose.
     pose.m_position += transform_t::m_translation;
+}
+transform_t transform_t::inverse() const
+{
+    // Create output transform.
+    // NOTE: This method allows Eigen to do operations in place.
+    transform_t inverted;
+
+    // Invert the translation.
+    inverted.m_translation = transform_t::m_translation * -1.0;
+
+    // Invert the rotation.
+    inverted.m_rotation = transform_t::m_rotation.inverse();
+
+    return inverted;
 }
