77f152dcb160ed31ada3bc6d59086da6c8a03726
Paul D'Angio <pcdangio@gmail.com>
2021-01-18 14:20:06 -0500

    model: initial commit of design structure

---
+++ include/kinematic_model/model/design.hpp
@@ -0,0 +1,33 @@
+#ifndef KINEMATIC_MODEL___MODEL___DESIGN_H
+#define KINEMATIC_MODEL___MODEL___DESIGN_H
+
+#include <kinematic_model/geometry/object/objects.hpp>
+#include <kinematic_model/geometry/attachment/attachments.hpp>
+
+#include <vector>
+#include <memory>
+
+namespace kinematic_model {
+namespace model {
+
+class design_t
+{
+public:
+    struct instruction_t
+    {
+        std::shared_ptr<geometry::object::object_t> object;
+        std::shared_ptr<geometry::object::object_t> parent;
+        std::shared_ptr<geometry::attachment::attachment_t> attachment;
+    };
+
+    std::vector<instruction_t> instructions() const;
+
+    bool add(const std::shared_ptr<geometry::object::object_t>& object, const std::shared_ptr<geometry::object::object_t>& parent, const std::shared_ptr<geometry::attachment::attachment_t>& attachment);
+
+private:
+    std::vector<instruction_t> m_instructions;
+};
+
+}}
+
+#endif
---
+++ src/kinematic_model/model/design.cpp
@@ -0,0 +1,52 @@
+#include <kinematic_model/model/design.hpp>
+
+#include <ros/console.h>
+
+using namespace kinematic_model::model;
+
+bool design_t::add(const std::shared_ptr<geometry::object::object_t>& object, const std::shared_ptr<geometry::object::object_t>& parent, const std::shared_ptr<geometry::attachment::attachment_t>& attachment)
+{
+    // Check that object exists.
+    if(!object)
+    {
+        ROS_ERROR("failed to add object to model design (object is nullptr)");
+        return false;
+    }
+
+    // Check that attachment exists.
+    if(!attachment)
+    {
+        ROS_ERROR_STREAM("failed to add object [" << object->name() << "] to model design (attachment is nullptr)");
+        return false;
+    }
+
+    // Check if parent exists
+    if(parent)
+    {
+        // Check that the parent exists in the design.
+        bool parent_exists = false;
+        for(auto instruction = design_t::m_instructions.cbegin(); instruction != design_t::m_instructions.cend(); ++instruction)
+        {
+            if(instruction->parent == parent)
+            {
+                parent_exists = true;
+                break;
+            }
+        }
+
+        // If this point reached and parent_exists is still false, parent does not exist.
+        if(!parent_exists)
+        {
+            ROS_ERROR_STREAM("failed to add object [" << object->name() << "] to model design (parent does not exist)");
+            return false;
+        }
+    }
+
+    // Add object to instructions.
+    design_t::m_instructions.push_back({object, parent, attachment});
+}
+
+std::vector<kinematic_model::model::design_t::instruction_t> design_t::instructions() const
+{
+    return design_t::m_instructions;
+}
