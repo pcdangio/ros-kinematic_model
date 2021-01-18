96474486b09d582d8e2eec5526fe04c9a30883b1
Paul D'Angio <pcdangio@gmail.com>
2021-01-18 14:19:31 -0500

    geometry: added top-level include header for objects and attachments

---
+++ include/kinematic_model/geometry/attachment/attachments.hpp
@@ -0,0 +1,8 @@
+#ifndef KINEMATIC_MODEL___GEOMETRY___ATTACHMENTS___ATTACHMENT_H
+#define KINEMATIC_MODEL___GEOMETRY___ATTACHMENTS___ATTACHMENT_H
+
+#include <kinematic_model/geometry/attachment/attachment.hpp>
+#include <kinematic_model/geometry/attachment/fixed.hpp>
+#include <kinematic_model/geometry/attachment/dynamic.hpp>
+
+#endif
---
+++ include/kinematic_model/geometry/object/objects.hpp
@@ -0,0 +1,11 @@
+#ifndef KINEMATIC_MODEL___GEOMETRY___OBJECT___OBJECTS_H
+#define KINEMATIC_MODEL___GEOMETRY___OBJECT___OBJECTS_H
+
+#include <kinematic_model/geometry/object/object.hpp>
+#include <kinematic_model/geometry/object/link.hpp>
+#include <kinematic_model/geometry/object/frame.hpp>
+#include <kinematic_model/geometry/object/joint/joint.hpp>
+#include <kinematic_model/geometry/object/joint/hinge.hpp>
+#include <kinematic_model/geometry/object/joint/prismatic.hpp>
+
+#endif
