a16cc441af4b01cc57c7a6d58b488e96fe494e0d
Paul D'Angio <pcdangio@gmail.com>
2021-01-18 16:18:21 -0500

    attachment: made fixed attachment constructor use euler angles

--- include/kinematic_model/geometry/attachment/fixed.hpp
+++ include/kinematic_model/geometry/attachment/fixed.hpp
@@ -12,7 +12,7 @@ class fixed_t
 {
 public:
     fixed_t(double x, double y, double z, double qw, double qx, double qy, double qz);
-    static std::shared_ptr<fixed_t> create(double x, double y, double z, double qw, double qx, double qy, double qz);
+    static std::shared_ptr<fixed_t> create(double x, double y, double z, double roll, double pitch, double yaw);
 
     transform_t get_transform(const Eigen::VectorXd& state_vector) const override;
 
--- src/kinematic_model/geometry/attachment/fixed.cpp
+++ src/kinematic_model/geometry/attachment/fixed.cpp
@@ -9,9 +9,15 @@ fixed_t::fixed_t(double x, double y, double z, double qw, double qx, double qy, 
     // Create the attachment's fixed transform.
     fixed_t::m_transform = transform_t({x, y, z}, {qw, qx, qy, qz});
 }
-std::shared_ptr<fixed_t> fixed_t::create(double x, double y, double z, double qw, double qx, double qy, double qz)
+std::shared_ptr<fixed_t> fixed_t::create(double x, double y, double z, double roll, double pitch, double yaw)
 {
-    return std::make_shared<fixed_t>(x, y, z, qw, qx, qy, qz);
+    // Create quaternion from euler rotations:
+    Eigen::Quaterniond orientation = Eigen::AngleAxisd(roll, Eigen::Vector3d::UnitX()) *
+                                     Eigen::AngleAxisd(pitch, Eigen::Vector3d::UnitY()) *
+                                     Eigen::AngleAxisd(yaw, Eigen::Vector3d::UnitZ());
+
+    // Create shared pointer.
+    return std::make_shared<fixed_t>(x, y, z, orientation.w(), orientation.x(), orientation.y(), orientation.z());
 }
 
 // METHODS
