7d0f3fc75a6ed99b02e7c9217eb7afe1b63666df
Paul D'Angio <pcdangio@gmail.com>
2021-01-19 00:20:36 -0500

    model_plugin: changed to reference of design in build method

--- include/kinematic_model/model_plugin.hpp
+++ include/kinematic_model/model_plugin.hpp
@@ -12,7 +12,7 @@ class model_plugin_t
 public:
     static std::shared_ptr<model_plugin_t> load(const std::string& plugin_path);
     
-    virtual bool build(const std::shared_ptr<geometry::design_t>& design) = 0;
+    virtual bool build(geometry::design_t& design) = 0;
 };
 
 #define REGISTER_MODEL_PLUGIN(class_name) extern "C" std::shared_ptr<kinematic_model::model_plugin_t> create_model_plugin() {return std::make_shared<class_name>();}
