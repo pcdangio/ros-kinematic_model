1d10c70e049685e1599ae30e16355d56c98e775d
Paul D'Angio <pcdangio@gmail.com>
2021-10-07 10:30:16 -0400

    geometry: added dtdr attachment

---
+++ include/kinematic_model/geometry/attachment/dtdr.hpp
@@ -0,0 +1,51 @@
+/// \file kinematic_model/geometry/attachment/dtdr.hpp
+/// \brief Defines the kinematic_model::geometry::attachment::dtdr_t class.
+#ifndef KINEMATIC_MODEL___GEOMETRY___ATTACHMENT___DTDR_H
+#define KINEMATIC_MODEL___GEOMETRY___ATTACHMENT___DTDR_H
+
+#include <kinematic_model/geometry/attachment/attachment.hpp>
+
+namespace kinematic_model {
+namespace geometry {
+namespace attachment {
+
+/// \brief A dynamic translation, dynamic rotation (DTFR) attachment between two objects.
+class dtdr_t
+    : public attachment_t
+{
+public:
+    // CONSTRUCTORS
+    /// \brief Instantiates a new DTFR attachment object.
+    /// \param state_index_x The state vector index of the translation x component.
+    /// \param state_index_y The state vector index of the translation y component.
+    /// \param state_index_z The state vector index of the translation z component.
+    /// \param state_index_qw The state vector index of the quaternion rotation w component.
+    /// \param state_index_qx The state vector index of the quaternion rotation x component.
+    /// \param state_index_qy The state vector index of the quaternion rotation y component.
+    /// \param state_index_qz The state vector index of the quaternion rotation z component.
+    dtdr_t(uint32_t state_index_x, uint32_t state_index_y, uint32_t state_index_z, uint32_t state_index_qw, uint32_t state_index_qx, uint32_t state_index_qy, uint32_t state_index_qz);
+
+    // METHODS
+    transform::transform_t get_transform(const Eigen::VectorXd& state_vector) const override;
+
+private:
+    // VARIABLES
+    /// \brief The state vector index of the dynamic x component of the translation.
+    uint32_t m_state_index_x;
+    /// \brief The state vector index of the dynamic y component of the translation.
+    uint32_t m_state_index_y;
+    /// \brief The state vector index of the dynamic z component of the translation.
+    uint32_t m_state_index_z;
+    /// \brief The state vector index of the dynamic w component of the quaternion.
+    uint32_t m_state_index_qw;
+    /// \brief The state vector index of the dynamic x component of the quaternion.
+    uint32_t m_state_index_qx;
+    /// \brief The state vector index of the dynamic y component of the quaternion.
+    uint32_t m_state_index_qy;
+    /// \brief The state vector index of the dynamic z component of the quaternion.
+    uint32_t m_state_index_qz;
+};
+
+}}}
+
+#endif
---
+++ src/kinematic_model/geometry/attachment/dtdr.cpp
@@ -0,0 +1,39 @@
+#include <kinematic_model/geometry/attachment/dtdr.hpp>
+
+using namespace kinematic_model::geometry::attachment;
+
+// CONSTRUCTORS
+dtdr_t::dtdr_t(uint32_t state_index_x, uint32_t state_index_y, uint32_t state_index_z, uint32_t state_index_qw, uint32_t state_index_qx, uint32_t state_index_qy, uint32_t state_index_qz)
+    : dtdr_t::attachment_t(attachment_t::type_t::DTDR)
+{
+    // Store translation indices.
+    dtdr_t::m_state_index_x = state_index_x;
+    dtdr_t::m_state_index_y = state_index_y;
+    dtdr_t::m_state_index_z = state_index_z;
+
+    // Store rotation indices.
+    dtdr_t::m_state_index_qw = state_index_qw;
+    dtdr_t::m_state_index_qx = state_index_qx;
+    dtdr_t::m_state_index_qy = state_index_qy;
+    dtdr_t::m_state_index_qz = state_index_qz;
+}
+
+// METHODS
+transform::transform_t dtdr_t::get_transform(const Eigen::VectorXd& state_vector) const
+{
+    // Create translation.
+    Eigen::Vector3d position;
+    position.x() = state_vector[dtdr_t::m_state_index_x];
+    position.y() = state_vector[dtdr_t::m_state_index_y];
+    position.z() = state_vector[dtdr_t::m_state_index_z];
+
+    // Create rotation.
+    Eigen::Quaterniond orientation;
+    orientation.w() = state_vector[dtdr_t::m_state_index_qw];
+    orientation.x() = state_vector[dtdr_t::m_state_index_qx];
+    orientation.y() = state_vector[dtdr_t::m_state_index_qy];
+    orientation.z() = state_vector[dtdr_t::m_state_index_qz];
+
+    // Return transform.
+    return transform::transform_t(position, orientation);
+}
